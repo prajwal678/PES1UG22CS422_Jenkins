68838584be983d896b0ecb5115e1c16057bcc69f
Prajwal <i.am.prajwal67@gmail.com>
2025-03-18 15:06:01 +0530

    t2

--- main/hello.cpp
+++ main/hello.cpp
@@ -1,4 +1,4 @@
-//File: hello.cpp
+// File: hello.cpp
 #include <iostream>
 using namespace std;
 
@@ -6,5 +6,6 @@ int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
+  cout << "Prajwal M (PES1UG22CS422)\n";
   return 0;
 }
